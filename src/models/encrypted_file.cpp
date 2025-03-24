7ac445bafd0191c3ac060f622b880d66bc3be846
dicapisar <dicapisar@gmail.com>
2025-03-24 20:13:07 +1030

    correction in constructor signature

--- src/models/encrypted_file.cpp
+++ src/models/encrypted_file.cpp
@@ -5,7 +5,7 @@
 #include "encrypted_file.h"
 
 
-EncryptedFile::EncryptedFile() : id(0), file_name(""), file_path(""), last_modified(0), password("") {}
+EncryptedFile::EncryptedFile() : id(0), file_name(""), file_path(""), last_modified(""), password("") {}
 
 EncryptedFile::EncryptedFile(int id, string file_name, string file_path, string last_modified, string password, User owner)
     : id(id), file_name(file_name), file_path(file_path), last_modified(last_modified), password(password), owner(owner) {}
