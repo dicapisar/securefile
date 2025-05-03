ca836f8021286f25de889eae4f04a616ee5a2697
dicapisar <dicapisar@gmail.com>
2025-05-03 15:31:50 +0930

    Add base of File Manager

--- src/core/file_management/file_management.cpp
+++ src/core/file_management/file_management.cpp
@@ -1,1 +1,35 @@
 #include "file_management.h"
+
+#include "./services/encrypt.h"
+#include "./services/file.h"
+#include "./services/database.h"
+#include "./models/session.h"
+
+#include <string>
+using namespace std;
+
+FileManagement::FileManagement(const DatabaseService& database, const EncryptService& encrypt, const FileService& file)
+    : database(database), encrypt(encrypt), file(file) {}
+
+FileManagement::~FileManagement() = default;
+
+
+bool FileManagement::encryptFile(Session& session, const string& file_name, const string& password) {
+    return true;
+}
+
+bool FileManagement::decryptFile(Session& session, const string& file_name, const string& password) {
+    return true;
+}
+
+bool FileManagement::deleteFile(Session& session, const string& file_name) {
+    return true;
+}
+
+bool FileManagement::listFiles(Session& session) {
+    return true;
+}
+
+bool FileManagement::shareFile(Session& session, const string& file_name, const string& student_id) {
+    return true;
+}
--- src/core/file_management/file_management.h
+++ src/core/file_management/file_management.h
@@ -1,12 +1,30 @@
 #ifndef FILE_MANAGEMENT_H
 #define FILE_MANAGEMENT_H
 
+#include "./services/encrypt.h"
+#include "./services/file.h"
+#include "./services/database.h"
+#include "./models/session.h"
+
+#include <string>
+using namespace std;
 
 
 class FileManagement {
-public:
-    FileManagement() = default;
-    ~FileManagement() = default;
+    private:
+        DatabaseService database;
+        EncryptService encrypt;
+        FileService file;
+
+    public:
+        bool encryptFile(Session& session, const string& file_name, const string& password);
+        bool decryptFile(Session& session, const string& file_name, const string& password);
+        bool deleteFile(Session& session, const string& file_name);
+        bool listFiles(Session& session);
+        bool shareFile(Session& session, const string& file_name, const string& student_id);
+
+        FileManagement(const DatabaseService& database, const EncryptService& encrypt, const FileService& file);
+        ~FileManagement();
 };
 
 
